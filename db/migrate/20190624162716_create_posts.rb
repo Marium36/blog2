c95dbe708b3eac5a1562a11ff85d477ef3a39092
Ruby <ruby@ubuntu.com>
2019-06-24 13:27:05 -0400

    Tried adding default values but rake db:migrate still fails

--- db/migrate/20190624162716_create_posts.rb
+++ db/migrate/20190624162716_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :title, default:"Title"
-      t.text :body, default:"Body"
+      t.string :title, default: "Title"
+      t.text: body, default: "Body"
 
       t.timestamps
     end
   end
-end
+end  
--- db/migrate/20190624165348_create_users.rb
+++ db/migrate/20190624165348_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :first_name, default:"FirstName"
-      t.string :last_name, default:"LastName"
+      t.string :first_name, default: "FirstName"
+      t.string :last_name, default: "LastName"
 
       t.timestamps
     end
-  ends
+  end
 end
