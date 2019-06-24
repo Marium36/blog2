47292e68dbcb6fe51cb557516e923669091fcf10
Ruby <ruby@ubuntu.com>
2019-06-24 12:45:59 -0400

    Modified migration files

--- db/migrate/20190624162716_create_posts.rb
+++ db/migrate/20190624162716_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :body
+      t.string :title, default:"Title"
+      t.text :body, default:"Body"
 
       t.timestamps
     end
--- db/migrate/20190624162748_create_comments.rb
+++ db/migrate/20190624162748_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.references :post, null: false, foreign_key: true
-      t.text :body
+      t.references :post, default: "Post", null: false, foreign_key: true
+      t.text :body, default: "Body"
 
       t.timestamps
     end
